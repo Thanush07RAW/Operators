a15b14e18c4d1964b98888bcb36ec07437590eb8
Thanush07RAW <draken0067@gmail.com>
2025-01-29 21:55:17 +0530

    Add files via upload

---
+++ Operatorstypes.java
@@ -0,0 +1,16 @@
+public class Operatorstypes {
+    public static void main(String[] args) {
+        int p = 18, q = 12;
+
+        // Arithmetic
+        System.out.println("Addition: " + (p + q));
+        System.out.println("Subtraction: " + (p - q));
+
+        // Relational
+        System.out.println("a < b: " + (p < q));
+
+        // Logical
+        System.out.println("a < b && b > 15: " + (p < q && q > 15));
+    }
+}
+
